0b7abf1671c313c99cb795d6a144561dfd9e8145
Pierre <piercel.coder2022@gmail.com>
2022-10-15 12:25:04 +0200

    Implement the solution

--- challenge.rb
+++ challenge.rb
@@ -1,6 +1,21 @@
 
 def balanced_brackets?(string)
   # your code here
+  return false if string.length.odd?
+  return false if string =~ /[^\[\]\(\)\{\}]/
+
+  pairs = { '{' => '}', '[' => ']', '(' => ')' }
+
+  stack = []
+  string.chars do |bracket|
+    if expectation = pairs[bracket]
+      stack << expectation
+    else
+      return false unless stack.pop == bracket
+    end
+  end
+
+  stack.empty?
 end
 
 puts balanced_brackets?('(hello)[world]')
